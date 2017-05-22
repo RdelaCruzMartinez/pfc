5bc4d939fe0ed9e3c934597287f3a29790e70770
Alexi <ramondelacruzmartinez@gmail.com>
2017-05-22 15:53:11 +0200

    qpx client

---
+++ mynba_v_0_0_1/mynba/controllers/flights/google_qpx_client.js
@@ -0,0 +1,34 @@
+module.exports =  {
+    findFligthsProcess: function (req) {
+        console.log("req.body.team => " + req.body.team);
+        console.log("req.body.origin => " + req.body.origin);
+        console.log("req.body.passengers => " + req.body.passengers);
+        console.log("req.body.fecIni => " + req.body.fecIni);
+        console.log("req.body.fecFin => " + req.body.fecFin);
+        //var json = this.buildRequest(req);
+        //this.makeRequest(json);
+    },
+    buildRequest: function (req) {
+        var postRequest = {
+            request: {
+                passengers: {
+                    adultCount: req.body.passengers
+                },
+                slice: [{
+                    origin: req.body.origin,
+                    destination: req.body.team,
+                    date: req.body.fecIni
+                },
+                {
+                    origin: req.body.team,
+                    destination: req.body.origin,
+                    date: req.body.fecFin
+                }],
+                solutions: 4
+            }
+        }
+    },
+    makeRequest: function (json) {
+        //TODO
+    }
+};
